b587cce4d3ae6bdd5585665f0117b62c0db3cca1
Erik Marks <rekmarks@protonmail.com>
2022-08-07 16:34:10 -0700

    Fix pr creation again again

--- scripts/fetch-rebuild-push.sh
+++ scripts/fetch-rebuild-push.sh
@@ -30,6 +30,7 @@ git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
 git checkout -b "${BRANCH_NAME}"
+git push --set-upstream origin "${BRANCH_NAME}"
 
 git add .
 git commit -m "${PR_TITLE}"
